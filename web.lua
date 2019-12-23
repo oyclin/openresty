709d4e48b4b4ded9862089f633eaad57fbddc8b4
UnderMoon <oyclin@163.com>
2019-12-23 16:03:56 +0800

    Update web.lua

--- web.lua
+++ web.lua
@@ -13,36 +13,39 @@ if not wsocket then
     return ngx.exit(444)
 end
 
+wsocket:send_text("connecting to the port 23 ... \n")
+
 local webtelnet = dofile ("webtelnet.lua")
---NOTE: 不会根据package.path进行搜索，而是根据相对路径搜索，而此处的应该是相对openresty的解释器的相对路径(openresty根目录)
-webtelnet.connect()
-while true do
-    local auto_out = webtelnet.input() --NOTE:就算不需要自动登录，这一行也不能注释掉。
-    if auto_out ~= "" then
-        if string.find(auto_out, "ogin:") then
-            auto_out = webtelnet.input(username)
-        end
-        if string.find(auto_out, "assword:") then
-            auto_out = webtelnet.input(passsword)
-            break
-        end
-    end
+--NOTE: 不会根据package.path进行搜索，而是根据相对路径搜索，而此处的应该是相对此nginx进程的相对路径(openresty根目录)
+if not webtelnet.connect() then
+    ngx.log(ngx.ERR, "failed to connect port 23")
+    wsocket:send_text("failed to connect telnet！\n")
+    wsocket:send_close()
+    return ngx.exit(444)
 end
 
-local function ascii_str(res)
-    local byte_s = ""
-    for i = 1, #res do
-        local b = string.byte(string.sub(res, i, i))
-        if b < 127 then
-            byte_s = byte_s .. b .. "-"
-        end
-    end
-    return byte_s
-end
+wsocket:send_text("connect success.\n")
 
+-- local welcome
+-- while true do
+    local auto_out = webtelnet.input() --NOTE: 这一段是自动登录，就算不需要，也不能注释这一行
+--     if auto_out ~= "" then
+--         if string.find(auto_out, "ogin:") then
+--             auto_out = webtelnet.input(username .. "\r\n")
+--         end
+--         if string.find(auto_out, "assword:") then
+--             welcome = webtelnet.input(passsword .. "\r\n")
+--             break
+--         end
+--     end
+-- end
+
+-- wsocket:send_text(welcome)
+
+-- local tellog = require "tellog"
+local skip = true
 while true do
     local data, typ, err = wsocket:recv_frame()
-
     if wsocket.fatal then
         ngx.log(ngx.ERR, "failed to receive frame: ", err)
         return ngx.exit(444)
@@ -65,8 +68,17 @@ while true do
             break
         end
 
-        local ascii = ascii_str(tostring(res))
-        etype, bytes, err = "text", wsocket:send_text("from nginx:" ..  tostring(ascii) .. "\n") --NOTE:如果是纯转发，那么不需要这个\n。（这里好像也不需要？）
+        -- if skip then
+        --     res = res .. tellog.getAll()
+        --     skip = nil
+        -- else
+        --     tellog.addLog(res)
+        -- end
+
+        --NOTE: 如果websocket是纯转发，那么这里就需要注释掉，如果是堆积发送，那么不需要注释
+        -- res = string.gsub(res, data, "", 1)
+
+        etype, bytes, err = "text", wsocket:send_text(res)
     end
 
     if not bytes then
